35fce213132e4abb56278b09c7f7d09a02e7c3f1
Cyril Muchiri <cyrilmuchiri11@gmail.com>
2023-04-27 23:41:12 +0300

    Initial commit

---
+++ src/practice/StringBuilderTest.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class StringBuilderTest {
+    public static void main(String []args){
+        StringBuilder string=new StringBuilder("Java");
+        System.out.println(string);
+
+        string.append(" strings tutorial");// adds string literals at the end of the original string
+
+        System.out.println(string);
+        //string.delete
+        //string.insert
+        //string.reverse
+        //string.replace
+
+    }
+}
